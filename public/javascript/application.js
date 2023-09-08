9d01846f65c934840185aae27ed7a2a15bdb138e
Alberto Fernandez-Capel <alberto@hey.com>
2023-09-08 13:47:55 +0100

    Include Turbo and Stimulus via importmaps

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,6 +1,6 @@
-import "/dist/turbo.es2017-umd.js"
-import { Application } from "/dist/stimulus.js"
-import "/vendor/strada.js"
+import "@hotwired/turbo"
+import { Application } from "@hotwired/stimulus"
+import "./vendor/strada.js"
 
 import FormController from "./controllers/bridge/form_controller.js"
 
